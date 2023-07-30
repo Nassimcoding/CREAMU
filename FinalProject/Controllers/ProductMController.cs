668f728089673b078caac382e953a3a9f92dc4e5
Tzu Chin <118542088+tzuchin1126@users.noreply.github.com>
2023-07-30 10:01:58 +0800

    7/30 商品狀態查詢

--- FinalProject/Controllers/ProductMController.cs
+++ FinalProject/Controllers/ProductMController.cs
@@ -9,45 +9,60 @@ namespace FinalProject.Controllers
 {
     public class ProductMController : Controller
     {
-
+        // 宣告私有變數用於儲存環境設定
         private IWebHostEnvironment _enviro = null;
         private IWebHostEnvironment _images;
 
+        // 建構子，注入環境設定
         public ProductMController(IWebHostEnvironment p, IWebHostEnvironment images)
         {
             _enviro = p;
             _images = images;
         }
 
-        public ActionResult ProductIndex(CKeywordViewModel vm, string keyword = "", decimal minPrice = 0, decimal maxPrice = decimal.MaxValue, int page = 1)
+        //顯示商品清單並支援篩選功能
+        public ActionResult ProductIndex(CKeywordViewModel vm, string keyword = "", string productStatus = "all", decimal minPrice = 0, decimal maxPrice = decimal.MaxValue, int page = 1)
         {
-            int pageSize = 10; //一頁顯示10樣商品
-            IQueryable<Product> datas = SearchProducts(keyword, minPrice, maxPrice); //關鍵字＋價格篩選
+            // 分頁設定
+            int pageSize = 10;
+            IQueryable<Product> datas = SearchProducts(keyword, minPrice, maxPrice);
 
+            // 根據選擇的商品狀態篩選商品
+            if (!string.IsNullOrEmpty(productStatus) && productStatus != "all")
+            {
+                datas = datas.Where(p => p.ProductStatus == productStatus);
+            }
+
+            // 根據用戶輸入的關鍵字進行篩選
             if (!string.IsNullOrEmpty(vm.txtKeyword))
+            {
                 datas = datas.Where(p => p.ProductName.Contains(vm.txtKeyword));
+            }
 
-            datas = datas.OrderByDescending(p => p.ReleaseDate); // 商品降序排列
+            // 商品按照發布日期降序排列
+            datas = datas.OrderByDescending(p => p.ReleaseDate);
 
+            // 執行分頁以顯示特定頁面的商品
             int recordCount = datas.Count();
             int totalPages = (int)Math.Ceiling((double)recordCount / pageSize);
             datas = datas.Skip((page - 1) * pageSize).Take(pageSize);
 
-            ViewBag.Page = page; // 將當前頁數放進 ViewBag
-            ViewBag.pageNum = totalPages;
+            ViewBag.Page = page; // 當前頁數
+            ViewBag.pageNum = totalPages; // 總頁數
             return View(datas.ToList());
         }
 
+        //顯示特定商品的編輯視圖
         public ActionResult Edit(int? id)
         {
             if (id == null)
-                return RedirectToAction("List");
+                return RedirectToAction("List"); // 如果未提供商品 ID，則重定向到列表頁面
 
             CreamUdbContext db = new CreamUdbContext();
             Product prod = db.Products.FirstOrDefault(p => p.ProductId == id);
 
 
-            //獲取商品狀態的選項數據
+            // 創建商品狀態的下拉選項數據
             var productStatusOptions = new List<SelectListItem>
             {
                 new SelectListItem { Value = "上架中", Text = "上架中" },
@@ -58,6 +73,7 @@ namespace FinalProject.Controllers
             ViewBag.Categories = productStatusOptions;
             return View(prod);
         }
+        // HTTP POST 動作：處理商品編輯表單提交
         [HttpPost]
         [ValidateAntiForgeryToken] //用於防止跨站點請求偽造攻擊（CSRF）
 
@@ -100,53 +116,62 @@ namespace FinalProject.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(Product t, IFormFile photo)
         {
-            if (ModelState.IsValid)
+            try
             {
-                if (photo == null)
-                    ModelState.AddModelError("photo", "請選擇商品圖片");
-                else if (!IsImageValid(photo))
-                    ModelState.AddModelError("photo", "圖片格式不正確，只支援 JPG、PNG、GIF 格式。");
-
-                if (IsProductDuplicate(t.ProductName))
-                    ModelState.AddModelError("ProductName", "商品名稱已存在。");
-                else if (t.ProductName.Length < 3)
-                    ModelState.AddModelError("ProductName", "商品名稱必須至少包含3個字元。");
-                else if (t.ProductName.Length > 60)
-                    ModelState.AddModelError("ProductName", "商品名稱不能超過60個字元。");
-
-                if (t.ProductStock <= 0)
-                    ModelState.AddModelError("ProductStock", "庫存數量必須大於零。");
-
-                if (t.Price <= 0)
-                    ModelState.AddModelError("Price", "價格必須大於零。");
-
                 if (ModelState.IsValid)
                 {
-                    t.ReleaseDate = DateTime.Now.ToString("yyyy-MM-dd"); // 設定 ReleaseDate 為當下日期，紀錄修改商品資訊的日期
+                    // 驗證表單數據的有效性
+                    if (photo == null)
+                        ModelState.AddModelError("photo", "請選擇商品圖片");
+                    else if (!IsImageValid(photo))
+                        ModelState.AddModelError("photo", "圖片格式不正確，只支援 JPG、PNG、GIF 格式。");
 
-                    if (photo != null) // 上傳圖片
+                    if (IsProductDuplicate(t.ProductName))
+                        ModelState.AddModelError("ProductName", "商品名稱已存在。");
+                    else if (t.ProductName.Length < 3)
+                        ModelState.AddModelError("ProductName", "商品名稱必須至少包含3個字元。");
+                    else if (t.ProductName.Length > 60)
+                        ModelState.AddModelError("ProductName", "商品名稱不能超過60個字元。");
+
+                    if (t.ProductStock <= 0)
+                        ModelState.AddModelError("ProductStock", "庫存數量必須大於零。");
+
+                    if (t.Price <= 0)
+                        ModelState.AddModelError("Price", "價格必須大於零。");
+
+                    // 如果驗證通過，則執行商品創建邏輯
+                    if (ModelState.IsValid)
                     {
-                        string photoName = Guid.NewGuid().ToString() + ".jpg";
-                        string imagePath = Path.Combine(_images.WebRootPath, "images", photoName);
-                        using (var stream = new FileStream(imagePath, FileMode.Create))
-                            photo.CopyTo(stream);
-                        t.ProductImage = photoName;
+                        t.ReleaseDate = DateTime.Now.ToString("yyyy-MM-dd"); // 設定 ReleaseDate 為當下日期，紀錄修改商品資訊的日期
+
+                        if (photo != null) // 上傳圖片
+                        {
+                            string photoName = Guid.NewGuid().ToString() + ".jpg";
+                            string imagePath = Path.Combine(_images.WebRootPath, "images", photoName);
+                            using (var stream = new FileStream(imagePath, FileMode.Create))
+                                photo.CopyTo(stream);
+                            t.ProductImage = photoName;
+                        }
+
+                        // 儲存商品資料到資料庫
+                        CreamUdbContext db = new CreamUdbContext();
+                        db.Products.Add(t);
+                        db.SaveChanges();
+
+                        return RedirectToAction("ProductIndex");
                     }
-                    CreamUdbContext db = new CreamUdbContext();
-                    db.Products.Add(t);
-                    db.SaveChanges();
-                    return RedirectToAction("ProductIndex");
                 }
+
+                // 驗證失敗，重新顯示創建商品的視圖，並傳遞錯誤訊息給使用者
+                SetProductStatusOptions();
+                return View(t);
             }
-            // 驗證失敗
-            SetProductStatusOptions();
-            return View(t);
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "資料庫存取異常，請稍後再試或聯絡系統管理員。");
+                return View(t);
+            }
         }
-
-
-
-
-
 
 
 
@@ -166,18 +191,28 @@ namespace FinalProject.Controllers
             return RedirectToAction("ProductIndex");
         }
 
-        // 查詢
+
+        //加入資料庫存取異常處理
         private IQueryable<Product> SearchProducts(string keyword, decimal minPrice, decimal maxPrice)
         {
-            CreamUdbContext db = new CreamUdbContext();
-            IQueryable<Product> datas = db.Products;
+            try
+            {
+                CreamUdbContext db = new CreamUdbContext();
+                IQueryable<Product> datas = db.Products;
 
-            if (!string.IsNullOrEmpty(keyword))
-                datas = datas.Where(p => p.ProductName.Contains(keyword));
+                if (!string.IsNullOrEmpty(keyword))
+                    datas = datas.Where(p => p.ProductName.Contains(keyword));
 
-            datas = datas.Where(p => p.Price >= minPrice && p.Price <= maxPrice);
-            return datas;
+                datas = datas.Where(p => p.Price >= minPrice && p.Price <= maxPrice);
+                return datas;
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "資料庫存取異常，請稍後再試或聯絡系統管理員。");
+                throw; // 可以選擇重新拋出異常，讓上層處理異常
+            }
         }
+
 
         // 商品預覽
         public ActionResult ProductDetail(int id)
@@ -212,12 +247,14 @@ namespace FinalProject.Controllers
             ViewBag.Categories = productStatusOptions;
         }
 
+        // 輔助方法：檢查指定商品名稱是否已存在於資料庫
         private bool IsProductDuplicate(string productName)
         {
             CreamUdbContext db = new CreamUdbContext();
             return db.Products.Any(p => p.ProductName == productName);
         }
 
+        // 輔助方法：檢查上傳的圖片是否為有效格式（JPG、JPEG、PNG、GIF）
         private bool IsImageValid(IFormFile photo)
         {
             // 支援的圖片格式
